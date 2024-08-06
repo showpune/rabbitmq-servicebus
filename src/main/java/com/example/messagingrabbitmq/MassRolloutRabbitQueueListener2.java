ff487f8cc99bcc426707e0152ee42bc2f3c6078e
showpune <iamlizhiyong@outlook.com>
2024-08-06 17:18:22 +0800

    gitignore

--- src/main/java/com/example/messagingrabbitmq/MassRolloutRabbitQueueListener2.java
+++ src/main/java/com/example/messagingrabbitmq/MassRolloutRabbitQueueListener2.java
@@ -1,10 +1,9 @@
 package com.example.messagingrabbitmq;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-//import org.springframework.amqp.rabbit.listener.exception.AmqpRejectAndDontRequeueException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 
 @Component
 @Slf4j
