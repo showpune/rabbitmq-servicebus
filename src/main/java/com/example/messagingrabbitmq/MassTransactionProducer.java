c3825384c82c42b5a494c93dcffe6964d2e1dc93
zhiyongli <iamlizhiyong@outlook.com>
2024-12-11 13:47:57 +0800

    migration-nokb

--- src/main/java/com/example/messagingrabbitmq/MassTransactionProducer.java
+++ src/main/java/com/example/messagingrabbitmq/MassTransactionProducer.java
@@ -1,14 +1,11 @@
 package com.example.messagingrabbitmq;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.azure.messaging.servicebus.ServiceBusClientBuilder;
+import com.azure.messaging.servicebus.ServiceBusSenderClient;
 import org.springframework.stereotype.Service;
-import org.springframework.amqp.core.MessageProperties;
-import org.springframework.amqp.core.Message;
 import org.springframework.beans.factory.annotation.Value;
 
 @Service
-//@Slf4j
-//@Profile("!gcp")
 public class MassTransactionProducer implements MessageProducer {
     private static final String MASS_DEPLOYMENT_RESPONSE_QUEUE = "FIRE-MASS-DEPLOYMENT-";
     private static final String FIRE_MASS_DEPLOYMENT_QUEUE = "FIRE-MASS-DEPLOYMENT-QUEUE-";
@@ -17,31 +14,31 @@ public class MassTransactionProducer implements MessageProducer {
     private static final String FIRE_FILTERED_MASS_DEPLOYMENT_QUEUE = "FIRE-FILTERED-MASS-DEPLOYMENT-QUEUE-";
     private static final String CREATE_UC_MANIFEST_REQUEST_QUEUE = "CREATE_UC_MANIFEST_REQUEST_QUEUE-";
     private static final String MASS_DEPLOY_RESULT_RESPONSE_QUEUE = "FIRE-MASS_DEPLOY_RESULT_RESPONSE_QUEUE-";
-    private final RabbitTemplate rabbitTemplate;
+    private final ServiceBusSenderClient senderClient;
     private final UserInfoService userInfoService;
-    @Value("${rabitMq.dcs.queue.name}")
-    private String DCSQueue;
+    @Value("${serviceBus.connectionString}")
+    private String connectionString;
     @Value("${currentEnvironment}")
     private String environment;
 
     LogUtil log;
 
-    public MassTransactionProducer(RabbitTemplate rabbitTemplate,
-                                   UserInfoService userInfoService) {
-        this.rabbitTemplate = rabbitTemplate;
+    public MassTransactionProducer(UserInfoService userInfoService) {
         this.userInfoService = userInfoService;
+        this.senderClient = new ServiceBusClientBuilder()
+                .connectionString(connectionString)
+                .sender()
+                .buildClient();
         log = new LogUtil();
     }
 
     @Override
-    public void sendMassRolloutRequest(RolloutDataAccessorRequest request){
-        //String responseString = toJsonThrowingException(request);
+    public void sendMassRolloutRequest(RolloutDataAccessorRequest request) {
         String responseString = "test";
         log.info("Sending mass rollout request: " + responseString);
-        MessageProperties properties = new MessageProperties();
-        properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        Message responseMessage = new Message(responseString.getBytes(), properties);
-        rabbitTemplate.convertAndSend(MASS_DEPLOYMENT_RESPONSE_QUEUE + environment, responseMessage);
+        senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                .setContentType("application/json")
+                .setSubject(MASS_DEPLOYMENT_RESPONSE_QUEUE + environment));
     }
 
     @Override
@@ -52,10 +49,9 @@ public class MassTransactionProducer implements MessageProducer {
         log.info("Queueing mass deployment request: " + request + " for user " + user);
         String responseString = toJsonThrowingException(request);
 
-        MessageProperties properties = new MessageProperties();
-        properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        Message responseMessage = new Message(responseString.getBytes(), properties);
-        rabbitTemplate.convertAndSend(FIRE_MASS_DEPLOYMENT_QUEUE + environment, responseMessage);
+        senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                .setContentType("application/json")
+                .setSubject(FIRE_MASS_DEPLOYMENT_QUEUE + environment));
     }
 
     private String toJsonThrowingException(Object request) {
@@ -67,42 +63,38 @@ public class MassTransactionProducer implements MessageProducer {
         log.info("Sending manifest orchestrator request: " + request);
         String responseString = toJsonThrowingException(request);
 
-        MessageProperties properties = new MessageProperties();
-        properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        Message responseMessage = new Message(responseString.getBytes(), properties);
-        rabbitTemplate.convertAndSend(MANIFEST_ORCHESTRATOR_RESPONSE_QUEUE + environment, responseMessage);
+        senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                .setContentType("application/json")
+                .setSubject(MANIFEST_ORCHESTRATOR_RESPONSE_QUEUE + environment));
     }
 
     @Override
     public void superPackageRequestDCs(SuperPackageDCRequest request) {
         log.info("Sending request to get DCs for super package: " + request);
         String responseString = toJsonThrowingException(request);
-        MessageProperties properties = new MessageProperties();
-        properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        Message responseMessage = new Message(responseString.getBytes(), properties);
-        rabbitTemplate.convertAndSend(FIRE_DCS_SUPER_PACKAGE + environment, responseMessage);
+
+        senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                .setContentType("application/json")
+                .setSubject(FIRE_DCS_SUPER_PACKAGE + environment));
     }
 
     @Override
     public void queueFilteredMassDeploymentRequest(SuperPackageDeployRequest request) {
         log.info("Sending request filtered mass deployment request: " + request);
         String responseString = toJsonThrowingException(request);
-        MessageProperties properties = new MessageProperties();
-        properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-        Message responseMessage = new Message(responseString.getBytes(), properties);
-        rabbitTemplate.convertAndSend(FIRE_FILTERED_MASS_DEPLOYMENT_QUEUE + environment, responseMessage);
+
+        senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                .setContentType("application/json")
+                .setSubject(FIRE_FILTERED_MASS_DEPLOYMENT_QUEUE + environment));
     }
 
     @Override
     public void sendDcsRequestToQueue(String dCSRequest) {
         log.info("DCS Request for queue : {}", dCSRequest);
         try {
-            MessageProperties properties = new MessageProperties();
-            properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-            Message responseMessage = new Message(dCSRequest.getBytes(), properties);
-            log.info("Sending message:" + dCSRequest);
-            rabbitTemplate.setDefaultReceiveQueue(DCSQueue);
-            rabbitTemplate.convertAndSend(DCSQueue, responseMessage);
+            senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(dCSRequest)
+                    .setContentType("application/json")
+                    .setSubject(DCSQueue));
             log.info("Message Published Successfully.");
         } catch (Exception exe) {
             CriticalAlert.rabbitMq("DCS Request :sendDcsRequestToQueue: Failure to send message to DCS Request to queue: " + exe.getMessage(), exe);
@@ -115,11 +107,9 @@ public class MassTransactionProducer implements MessageProducer {
         log.info("Sending update construct manifest request: " + request);
         String responseString = toJsonThrowingException(request);
         try {
-
-            MessageProperties properties = new MessageProperties();
-            properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-            Message responseMessage = new Message(responseString.getBytes(), properties);
-            rabbitTemplate.convertAndSend(CREATE_UC_MANIFEST_REQUEST_QUEUE + environment, responseMessage);
+            senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                    .setContentType("application/json")
+                    .setSubject(CREATE_UC_MANIFEST_REQUEST_QUEUE + environment));
             log.info("Sent Update Construct manifest creation request to MDO for " + ": " + request);
         } catch (Exception ex) {
             CriticalAlert.rabbitMq("Error posting Update Construct manifest orchestrator request on queue: "
@@ -137,15 +127,13 @@ public class MassTransactionProducer implements MessageProducer {
         log.info("Sending mass deployment result response: " + deployResponse.summarize());
         String responseString = toJsonThrowingException(deployResponse);
         try {
-            MessageProperties properties = new MessageProperties();
-            properties.setContentType(MessageProperties.CONTENT_TYPE_JSON);
-            Message responseMessage = new Message(responseString.getBytes(), properties);
-            rabbitTemplate.convertAndSend(MASS_DEPLOY_RESULT_RESPONSE_QUEUE + environment, responseMessage);
+            senderClient.sendMessage(new com.azure.messaging.servicebus.ServiceBusMessage(responseString)
+                    .setContentType("application/json")
+                    .setSubject(MASS_DEPLOY_RESULT_RESPONSE_QUEUE + environment));
             log.info("Sent Update Construct manifest creation request to MDO for " + ": " + deployResponse.summarize());
         } catch (Exception ex) {
             CriticalAlert.rabbitMq("Error posting Update Construct manifest orchestrator request on queue: "
                     + MASS_DEPLOY_RESULT_RESPONSE_QUEUE + " with message: " + responseString, ex);
         }
     }
-
 }
